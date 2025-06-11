f5e0e325560eb7254aab3235ee29e859040011f7
abid <abid.ttsit@example.com>
2025-06-11 15:28:37 +0530

    order create

--- app/Models/OrderItem.php
+++ app/Models/OrderItem.php
@@ -3,10 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Order;
 
 class OrderItem extends Model
 {
     protected $fillable = [
-        'size', 'meter', 'micron', 'color', 'qty', 'rate'
+        'user_id', 'order_id', 'size', 'meter', 'micron', 'color', 'qty', 'rate'
     ];
+
+    public function orders()
+    {
+        return $this->belongsTo(Order::class);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\api\user\AuthController;
+use App\Http\Controllers\api\user\OrderController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -8,6 +9,17 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+
 Route::controller(AuthController::class)->prefix('user')->group(function(){
      Route::post('login', 'login');
+});
+
+Route::middleware('auth:api')->group(function(){
+    Route::controller(OrderController::class)->prefix('user')->group(function(){
+        Route::post('orders', 'store');
+    });
+});
+
+Route::middleware('auth:api')->get('/test-token', function () {
+    return response()->json(['message' => 'Token is valid!']);
 });
